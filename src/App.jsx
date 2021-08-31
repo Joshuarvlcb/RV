8818e545f83c168f7cf0a26e77c3c09bf70942ac
John McFarland <Jmcfar187@west-mec.local>
2021-08-31 14:03:54 -0700

    pushing my stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Landing from './components/LandingPage'
+import Services from './components/Services';
 
 function App() {
   return (
     <div >
           <Landing/>
+          <Services />
     </div>
   );
 }
